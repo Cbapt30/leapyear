9443f01941db4db82a0f1c2c4a8139df7b83083d
Celia Baptiste and Craig Hutler <craig@>
2019-06-17 19:55:34 -0700

    completed javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
     var year = parseInt($("input#year").val());
 
     var leapYear = function(year) {
-      return false;
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
     };
-
-
 
 
     var result = leapYear(year);
