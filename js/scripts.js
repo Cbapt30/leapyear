6e0bcb3e5d4b6d1ae9c51521c639ecd662451f31
Celia Baptiste and Craig Hutler <craig@>
2019-06-17 20:17:52 -0700

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
+// Business Logic
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
+// User Interface Logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
-
-    var leapYear = function(year) {
-      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-
     var result = leapYear(year);
 
-    $("#result").text(result);
+    $(".year").text(year);
+
+    if (!result) {           // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+    //$("#result").text(result);
   });
 });
